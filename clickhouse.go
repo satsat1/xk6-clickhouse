aeadd2df37a7e3243961b933fcf5de7001887dd5
satsat1 <_sat@mail.ru>
2024-08-21 14:04:02 +0500

    Update clickhouse.go

--- clickhouse.go
+++ clickhouse.go
@@ -1,82 +1,27 @@
-package k6clickhouse
+package clickhouse
 
 import (
-	"context"
-	// "encoding/json"
-	"fmt"
-	"go.k6.io/k6/js/modules"
-
-	"github.com/ClickHouse/clickhouse-go/v2"
-	// "github.com/ClickHouse/clickhouse-go/v2/lib/driver"
-	// "github.com/mstoykov/envconfig"
-	// "go.k6.io/k6/lib/types"
-	// "gopkg.in/guregu/null.v4"
-	// "strings"
-	// "time"
+    "fmt"
+    "go.k6.io/k6/js/modules"
 )
 
+// init is called by the Go runtime at application startup.
 func init() {
-	modules.Register("k6/x/k6clickhouse", new(k6Clickhouse))
+    modules.Register("k6/x/clickhouse", new(Compare))
 }
 
-type k6Clickhouse struct{
-	clickConn		clickhouse.Conn
-	ctx			context.Context
+// Compare is the type for our custom API.
+type Compare struct{
+    ComparisonResult string // textual description of the most recent comparison
 }
 
-// type Client struct {
-// 	client *Clickhouse.Client
-// }
-
-func (cl *k6Clickhouse) Connect( host string, port int, database string, username string, password string ) error {
-	// clickConn, err := clickhouse.Open(connURI)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	clickConn, err := clickhouse.Open(&clickhouse.Options{
-		Addr: []string{fmt.Sprintf("%s:%d", host, port)},
-		Auth: clickhouse.Auth{
-			Database: database,
-			Username: username,
-			Password: password,
-		},
-	})
-	if err != nil {
-		return err
-	}
-	
-	cl.clickConn = clickConn
-	cl.ctx = context.Background()
-	
-	return nil
-	// return &Clickhouse{
-	// 	clickConn:	clickConn,
-	// 	ctx:		context.Background()
-	// }
+// IsGreater returns true if a is greater than b, or false otherwise, setting textual result message.
+func (c *Compare) IsGreater(a, b int) bool {
+    if a > b {
+        c.ComparisonResult = fmt.Sprintf("%d is greater than %d", a, b)
+        return true
+    } else {
+        c.ComparisonResult = fmt.Sprintf("%d is NOT greater than %d", a, b)
+        return false
+    }
 }
-
-func (cl *k6Clickhouse) Close() error {
-	err := cl.clickConn.Close()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (cl *k6Clickhouse) Insert(data string) error {
-	err := cl.clickConn.Exec(cl.ctx, data)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// func (cl *Clickhouse) Batch(data []byte) error {
-// 	_, err := cl.clickConn.Write(data)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
