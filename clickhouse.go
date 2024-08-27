06633236aaca1a04d43dbb21b887ec1f9ed4ed53
satsat1 <_sat@mail.ru>
2024-08-27 11:13:39 +0500

    Update clickhouse.go

--- clickhouse.go
+++ clickhouse.go
@@ -3,6 +3,7 @@ package k6clickhouse
 import (
 	"context"
 	"fmt"
+	"log"
 	"go.k6.io/k6/js/modules"
 	"github.com/ClickHouse/clickhouse-go/v2"
 )
@@ -62,25 +63,64 @@ func (c *Compare) Connect1( host string, port int, database string, username str
 	// if err != nil {
 	// 	return nil, err
 	// }
-	clickConn, err := clickhouse.Open(&clickhouse.Options{
-		Addr: []string{fmt.Sprintf("%s:%d", host, port)},
-		Auth: clickhouse.Auth{
-			Database: database,
-			Username: username,
-			Password: password,
-		},
-	})
+	var (
+		ctx       = context.Background()
+		clickConn, err := clickhouse.Open(&clickhouse.Options{
+			Addr: []string{fmt.Sprintf("%s:%d", host, port)},
+			Auth: clickhouse.Auth{
+				Database: database,
+				Username: username,
+				Password: password,
+			},
+		})
+	)
 	if err != nil {
-		return err
-	}
-	
-	//clickConn.Exec(context.Background(), data)
-	err1 := clickConn.Exec(context.Background(), data)
-	if err1 != nil {
-		return err1
+		log.Fatal(err)
 	}
 
+	if err := clickConn.Exec(ctx, data); err != nil {
+		log.Fatal(err)
+	}
+	
+	// //clickConn.Exec(context.Background(), data)
+	// err1 := clickConn.Exec(context.Background(), data)
+	// if err1 != nil {
+	// 	return err1
+	// }
+
 	return nil
+
+	// var (
+	// 	ctx       = context.Background()
+	// 	conn, err = clickhouse.Open(&clickhouse.Options{
+	// 		Addr: []string{"127.0.0.1:9000"},
+	// 		Auth: clickhouse.Auth{
+	// 			Database: "default",
+	// 			Username: "default",
+	// 			Password: "",
+	// 		},
+	// 		//Debug:           true,
+	// 		DialTimeout:     time.Second,
+	// 		MaxOpenConns:    10,
+	// 		MaxIdleConns:    5,
+	// 		ConnMaxLifetime: time.Hour,
+	// 	})
+	// )
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if err := conn.Exec(ctx, "DROP TABLE IF EXISTS benchmark_async"); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if err := conn.Exec(ctx, ddl); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// start := time.Now()
+	// if err := benchmark(conn); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(time.Since(start))
+	
 }
 
 func (c *Compare) Close() error {
